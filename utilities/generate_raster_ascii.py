2f7021d96b8edf1781da08f055ab18ef9ed62390
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2020-12-21 10:32:12 +0000

    Updates scripts for viewing raster files.

--- utilities/generate_raster_ascii.py
+++ utilities/generate_raster_ascii.py
@@ -2,10 +2,11 @@ import numpy as np
 
 # by default it will be created in ../data folder
 filename = 'dummy_raster.asc'
+filenameFeature = 'dummy_raster_feature.asc'
 
 # Change at will
-ncols = 8
-nrows = 5
+ncols = 50
+nrows = 80
 xllCorner = 0.0
 yllCorner = 0.0
 cellSize = 2.0
@@ -38,5 +39,34 @@ def generate_raster_ascii(fname, cols, rows, xll, yll, cell, nodata, low=0.1, hi
             w.write('\n')
 
 ############################################
+def generate_raster_ascii_with_feature(fname, cols, rows, xll, yll, cell, nodata, low=0.1, high=10.0):
+    '''
+    '''
+    with open('../data/' + fname, 'w') as w:
+        w.write(f'ncols \t {cols} \n')
+        w.write(f'nrows \t {rows} \n')
+        w.write(f'xllcorner \t {xll} \n')
+        w.write(f'yllcorner \t {yll} \n')
+        w.write(f'cellsize \t {cell} \n')
+        w.write(f'NODATA_value \t {nodata} \n')
+
+        temp = np.random.uniform(low=low, high=high, size=(rows,cols))
+        midcol = int(cols / 2)
+        
+        # Create an arrow-like shape
+        for i in range(1, 4):
+            temp[i][midcol - i] = nodata
+            temp[i][midcol + i] = nodata
+        for irow in range(rows):
+            temp[irow][midcol] = nodata
+
+        for irow in range(rows):
+            for icol in range(cols):
+                w.write('{:10.3f} '.format(temp[irow][icol]))    
+            w.write('\n')
+
+############################################
 if __name__ == '__main__':
     generate_raster_ascii(filename, ncols, nrows, xllCorner, yllCorner, cellSize, NODATA_d)
+    generate_raster_ascii_with_feature(filenameFeature, ncols, nrows, xllCorner, yllCorner, cellSize, NODATA_d)
+
--- utilities/show_raster_ascii.py
+++ utilities/show_raster_ascii.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 
 # by default it will read from ../data folder
 filename = 'dummy_raster.asc'
+filenameFeature = 'dummy_raster_feature.asc'
 
 ############################################
 def show_raster_ascii(fname):
@@ -19,19 +20,24 @@ def show_raster_ascii(fname):
     NODATA_d = float(lines[5].split()[1])
 
     data = np.zeros((nrows,ncols))
-    iline = 6
+    lineOffset = 6
     for irow in range(nrows):
-        line = lines[iline]
+        line = lines[irow + lineOffset]
         vals = line.split()
-        data[irow][:] = [float(val) for val in vals]
-    
+        for icol in range(ncols):
+            data[irow][icol] = float(vals[icol])
+            if data[irow][icol] <= NODATA_d:
+                data[irow][icol] = np.nan
+
     x = np.linspace(xllCorner, xllCorner + cellSize * (ncols - 1), ncols)
     y = np.linspace(yllCorner + cellSize * (nrows - 1), yllCorner, nrows)
     xv, yv = np.meshgrid(x, y)
     
     plt.pcolor(xv, yv, data)
+    #plt.pcolor(data)
     plt.show()
 
 ############################################
 if __name__ == '__main__':
-    show_raster_ascii(filename)
+    #show_raster_ascii(filename)
+    show_raster_ascii(filenameFeature)
