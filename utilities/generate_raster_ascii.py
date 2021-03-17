3d37d4f571ae8976cb96321126db9ffc43096559
Aristotelis.Liakatas <Aristotelis.Liakatas@jacobs.com>
2021-03-17 21:40:13 +0000

    Improves generation of example datasets with gradient and feature.

--- utilities/generate_raster_ascii.py
+++ utilities/generate_raster_ascii.py
@@ -3,6 +3,7 @@ import numpy as np
 # by default it will be created in ../data folder
 filename = 'dummy_raster.asc'
 filenameFeature = 'dummy_raster_feature.asc'
+filenameGradWithFeature = 'dummy_raster_grad_feature.asc'
 
 # Change at will
 ncols = 50
@@ -66,7 +67,51 @@ def generate_raster_ascii_with_feature(fname, cols, rows, xll, yll, cell, nodata
             w.write('\n')
 
 ############################################
+def generate_grid_with_gradient(cols, rows, xll, yll, cell, nodata, low=0.1, high=10.):
+    '''
+    '''
+    r = high - low
+    temp = np.zeros((rows, cols))
+    for irow in range(rows):
+        for icol in range(cols):
+            temp[irow, icol] = low + irow * r + icol * r
+    return temp
+
+############################################
+def embed_feature(cols, rows, data, nodataval):
+    '''
+    '''
+    midcol = int(cols / 2)
+        
+    # Create an arrow-like shape
+    for i in range(1, 4):
+        data[i][midcol - i] = nodataval
+        data[i][midcol + i] = nodataval
+    for irow in range(rows):
+        data[irow][midcol] = nodataval
+    return data
+
+############################################
+def write_to_ascii(fname, cols, rows, xll, yll, cell, nodata, data):
+    '''
+    '''
+    with open('../data/' + fname, 'w') as w:
+        w.write(f'ncols \t {cols} \n')
+        w.write(f'nrows \t {rows} \n')
+        w.write(f'xllcorner \t {xll} \n')
+        w.write(f'yllcorner \t {yll} \n')
+        w.write(f'cellsize \t {cell} \n')
+        w.write(f'NODATA_value \t {nodata} \n')
+        for irow in range(rows):
+            for icol in range(cols):
+                w.write('{:10.3f} '.format(data[irow][icol]))    
+            w.write('\n')
+
+############################################
 if __name__ == '__main__':
     generate_raster_ascii(filename, ncols, nrows, xllCorner, yllCorner, cellSize, NODATA_d)
     generate_raster_ascii_with_feature(filenameFeature, ncols, nrows, xllCorner, yllCorner, cellSize, NODATA_d)
 
+    grad_data = generate_grid_with_gradient(ncols, nrows, xllCorner, yllCorner, cellSize, NODATA_d)
+    grad_data = embed_feature(ncols, nrows, grad_data, NODATA_d)
+    write_to_ascii(filenameGradWithFeature, ncols, nrows, xllCorner, yllCorner, cellSize, NODATA_d, grad_data)
